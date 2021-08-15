d6e10d5e7597253177cb63e6f1f3acaba9b1bcc4
VioletViola <vivian.v18723@gmail.com>
2021-08-15 19:48:17 +0100

    Practice with concatenation using java.

---
+++ src/Concatenation.java
@@ -0,0 +1,9 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        String fName = "Cory";
+        String lName = "Baxter";
+        int age = 28;
+        System.out.println(fName + " " + lName + " is " + age + " years old!");
+        System.out.println(fName + " " + lName + "'s band had a song called \"Feelin' the love\"!");
+    }
+}
